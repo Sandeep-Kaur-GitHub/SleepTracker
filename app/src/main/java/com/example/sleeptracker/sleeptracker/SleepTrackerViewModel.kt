89acf8f8c9ff55c1d771d0ce018ed523dcc76030
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-08 22:01:12 +0530

    Add viewModel and ViewModel factory class

--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.sleeptracker.sleeptracker
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.sleeptracker.database.SleepDatabase
+import com.example.sleeptracker.database.SleepDatabaseDao
+
+class SleepTrackerViewModel (val databaseDao: SleepDatabaseDao,application: Application):AndroidViewModel(application)
