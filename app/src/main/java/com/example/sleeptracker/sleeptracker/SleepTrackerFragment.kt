1dd55318d7b1106ee52616003d9ad85989fc39c8
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-13 12:23:46 +0530

    Add coroutines for long running operations

--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -6,23 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.sleeptracker.R
+import com.example.sleeptracker.database.SleepDatabase
 import com.example.sleeptracker.databinding.FragmentSleepTrackerBinding
 
 
 class SleepTrackerFragment : Fragment() {
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
         inflater, R.layout.fragment_sleep_tracker, container, false)
-
+        val application= requireNotNull(this.activity).application
+        val dataSource=SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory=SleepTrackerViewModelFactory(dataSource, application)
+        val sleepTrackerViewModel=ViewModelProvider(this,viewModelFactory).get(SleepTrackerViewModel::class.java)
+        binding.lifecycleOwner=this
+        binding.sleeptrackerViewModel=sleepTrackerViewModel
         return binding.root
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_sleep_tracker, container, false)
     }
-
 }
--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -2,7 +2,73 @@ package com.example.sleeptracker.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.viewModelScope
+
 import com.example.sleeptracker.database.SleepDatabase
 import com.example.sleeptracker.database.SleepDatabaseDao
+import com.example.sleeptracker.database.SleepNight
+import com.example.sleeptracker.formatNights
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class SleepTrackerViewModel (val databaseDao: SleepDatabaseDao,application: Application):AndroidViewModel(application)
+class SleepTrackerViewModel(
+    val databaseDao: SleepDatabaseDao,
+    application: Application
+) : AndroidViewModel(application) {
+    private var tonight = MutableLiveData<SleepNight?>()
+    private var night = databaseDao.getAllNights()
+    val nightsString = Transformations.map(night) { nights ->
+        formatNights(nights, application.resources)
+    }
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        viewModelScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        var night = databaseDao.getTonight()
+        if (night?.endTimeMilli != night?.startTimeMilli) {
+            night = null
+        }
+        return night
+    }
+    fun onStartTracking()
+    {
+        viewModelScope.launch {
+            val newNight=SleepNight()
+            insert(newNight)
+            tonight.value=getTonightFromDatabase()
+        }
+    }
+    private suspend fun insert(night: SleepNight){
+        databaseDao.insert(night)
+    }
+
+    fun onStopTracking(){
+        viewModelScope.launch {
+            val oldNight=tonight?.value ?: return@launch
+            oldNight.endTimeMilli= System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+    private suspend fun update(night: SleepNight) {
+        databaseDao.update(night)
+    }
+    fun onClear() {
+        viewModelScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        databaseDao.clear()
+    }
+}
