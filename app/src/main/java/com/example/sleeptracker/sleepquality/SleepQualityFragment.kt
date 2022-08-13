81a497ee4ae0b83c4b864a2fba741a2cbc800dd5
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-13 17:22:10 +0530

    Record Sleep Quality

--- app/src/main/java/com/example/sleeptracker/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/sleeptracker/sleepquality/SleepQualityFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.sleeptracker.R
+import com.example.sleeptracker.database.SleepDatabase
 import com.example.sleeptracker.databinding.FragmentSleepQualityBinding
 
 class SleepQualityFragment : Fragment() {
@@ -19,7 +21,13 @@ class SleepQualityFragment : Fragment() {
             inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
-
+        val arguments = SleepQualityFragmentArgs.fromBundle(requireArguments())
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey, dataSource)
+        val sleepQualityViewModel =
+            ViewModelProvider(
+                this, viewModelFactory).get(SleepQualityViewModel::class.java)
+        binding.sleepQualityViewModel = sleepQualityViewModel
         return binding.root
 
     }
--- app/src/main/java/com/example/sleeptracker/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/sleepquality/SleepQualityViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.sleeptracker.sleepquality
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sleeptracker.database.SleepDatabaseDao
+import kotlinx.coroutines.launch
+
+class SleepQualityViewModel(
+    private val sleepNightKey: Long = 0L,
+    val database: SleepDatabaseDao
+) : ViewModel() {
+    fun onSetSleepQuality(quality: Int) {
+        viewModelScope.launch {
+            val tonight = database.get(sleepNightKey) ?: return@withContext
+            tonight.sleepQuality = quality
+            database.update(tonight)
+            _navigateToSleepTracker.value = true
+        }
+    }
+}
